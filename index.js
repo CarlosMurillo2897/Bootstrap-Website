eeffad95097d65098a4636407e6243ce80807048
Carlos Murillo <carlos.murillo@abrigo.com>
2021-08-13 12:05:24 -0600

    Adding JS initial script. Added Tool Tip functionality.

---
+++ index.js
@@ -0,0 +1,6 @@
+var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(
+    el => new bootstrap.Tooltip(el)
+);
